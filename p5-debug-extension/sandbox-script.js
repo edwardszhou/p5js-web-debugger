f17268fdb8ac9eb346f19eb5c0a679af1ae1cf61
ch3926 <ch3926@nyu.edu>
2023-11-13 16:16:10 -0500

    Modified 'sandbox-script.js' to experiment with 'injecting' code into debugger instance.

--- p5-debug-extension/sandbox-script.js
+++ p5-debug-extension/sandbox-script.js
@@ -2,8 +2,14 @@ window.addEventListener('message', async function (event) {
 
     let newData = `${event.data.trim().replace(/[\u200B-\u200D\uFEFF]/g, '')}`
     // let data2 = data.trim().replace(/[\u200B-\u200D\uFEFF]/g, '').split(/[\s,\t,\n]+/).join(' ');
-    
-    // console.log(newData);
+
+    // ----- start ----- these lines allow injecting to the start of the draw loop
+    console.log(newData);
+    let codeLines = codeList(newData)
+    newData = insertDrawStart("  background('red');", codeLines)
+    console.log(newData)
+    // ----- end -----
+
     // console.log(data2);
     // for(let i = 0; i < newData.length; i++) {
     //     if(newData.charAt(i) != data2.charAt(i)) {
@@ -24,6 +30,28 @@ window.addEventListener('message', async function (event) {
 
     document.body.appendChild(newScript);
     document.body.appendChild(newScript2);
-    
 });
 
+//generate a list holding each line of the code in newData
+function codeList(data) {
+    let codeLines = data.split("\n") //split based on "\n"
+    console.log(codeLines)
+    return codeLines
+}
+
+function findDrawStart(codeLines) {
+    const setupLine = "function draw() {"
+
+    for (i=0;i<codeLines.length;i++){
+        if (codeLines[i] == setupLine){
+            return i+1
+        }
+    }
+}
+
+function insertDrawStart(str, codeLines) {
+    console.log('inserting ' + str);
+    let insertIndex = findDrawStart(codeLines);
+    codeLines.splice(insertIndex, 0, str)
+    return codeLines.join("\n")
+}
